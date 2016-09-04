14a13a8ca243b2b59d2b1c8315ef33dd538655c4
Arif <Arif@DESKTOP-5SE8IOR>
2016-09-04 15:31:39 +0600

    Problem !!!!!!!! I want to fix it

--- src/main/java/com/arifulislam/SpringMvcProductsApplication.java
+++ src/main/java/com/arifulislam/SpringMvcProductsApplication.java
@@ -1,8 +1,13 @@
 package com.arifulislam;
 
+
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import com.arifulislam.dao.ProductRepositry;
 import com.arifulislam.entites.Product;
@@ -18,6 +23,42 @@ public class SpringMvcProductsApplication {
 		dao.save(new Product("Arif", 4000));
 		dao.save(new Product("Nok", 9000));
 		dao.save(new Product("jdud", 2000));
+		dao.save(new Product("Rkas", 430));
+		
+		
+		// Show Product list 
+		System.err.println("-----------------------------");
+		System.out.println("List item");
+		List<Product> prods=dao.findAll();
+		for(Product p:prods){
+			System.out.println(p.getDescription()+"---"+ p.getPrice());
+		}
+		
+		// Show Pagination and find all product also select only 4 product
+		
+		Page<Product> pageProducts=dao.findAll(new PageRequest(0, 4));
+		System.out.println("Page Counter: " +pageProducts.getNumber());
+		System.out.println("Total Page: "+pageProducts.getTotalPages() );
+		System.out.println("Number of Product :" +pageProducts.getTotalElements());
+		
+		List<Product> produc2=pageProducts.getContent();
+		for(Product p:produc2){
+			System.out.println(p.getDescription()+"" +p.getPrice());
+		}
+		
+	//Product p=dao.findOne(L1)
+		
+	System.err.println("-----------------------------");
+	System.out.println("Product find One(L1)");
+		Page<Product> produc3=dao.chercherProducts("%L%", new PageRequest(0, 2));
+		
+		for(Product p:produc3.getContent()){
+			System.out.println(p.getDescription()+"" +p.getPrice());
+		}
+		
+		
+		
+		
 		
 		
 	}
--- src/main/java/com/arifulislam/dao/ProductRepositry.java
+++ src/main/java/com/arifulislam/dao/ProductRepositry.java
@@ -1,11 +1,23 @@
 package com.arifulislam.dao;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.arifulislam.entites.Product;
 
 public interface ProductRepositry extends JpaRepository<Product, Long>{
 
+	public List<Product> findByDescription(String des);
+	public Page<Product> findByDescription(String des, Pageable pageable);
+	 
+	@Query("select p from product p where p.description like :x")
+	public Page<Product> chercherProducts(@Param("x") String mc,Pageable pageable);
+	
+	@Query("select p from product p where p.price >:x and p.price <:y")
+	public List<Product> products(@Param("x")double price1,@Param("y")double price2);
 	
 
 }
